c5f6dde480e95dcdc4e0eda976e760bcb8b60573
asix360 <alessandro_eddy@hotmail.com>
2015-10-16 10:00:49 -0300

    Update views.py
    corrigindo o erro no view

--- atendimentos/views.py
+++ atendimentos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from form import PacienteForm
-from django.shortcuts import render_to_response
+from .form import PacienteForm
+
 
 def poslogin(request):
 	
