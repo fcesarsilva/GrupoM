00ce06d2bb2225add51b84bdff1b6de0989c2f14
asix360 <alessandro_eddy@hotmail.com>
2015-11-18 14:14:25 -0300

    mudando o codigo do prontuario auto incremento

--- prontuarios/models.py
+++ prontuarios/models.py
@@ -5,7 +5,7 @@ from funcionarios.models import *
 from farmacos.models import * #importa todas as classes do modelo atendimento. Caso queira especificar
 								 #devemos tirar o "*" e colocar a classe desejada.
 class Prontuario(models.Model):
-        codigo_Prontuario = models.IntegerField()
+        codigo_Prontuario = models.AutoField(primary_key=True)
         data_Consulta = models.DateField()
         paciente = models.ForeignKey(Paciente)
         medico = models.ManyToManyField(Medico)
